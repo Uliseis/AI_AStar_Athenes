63ab2521264f2be705d2fc69ce8aac31f6329c5a
Ulises <ui.bertolo@alumnos.upm.es>
2020-12-04 10:54:47 +0100

    Graph.py

--- Graph.py
+++ Graph.py
@@ -8,7 +8,7 @@ with open("distancias.csv", "r") as file1:
     for row in reader1:
         distrows.append(row)
 
-for k in len(distrows):  # Añade al dictionary
+for k in range(0, len(distrows)):  # Añade al dictionary
     estfinal = distrows[k][0]
     estinicial = distrows[k][1]
     distap = int(distrows[k][2])
@@ -28,14 +28,14 @@ with open("estaciones.csv", "r") as file3:
         estrows.append(row)
 
 metro = nx.Graph()
-for i in len(estrows):  # Añade nodos
+for i in range(0, len(estrows)):  # Añade nodos
     nodo = estrows[i][0]
     line = list()
     for j in range (1, len(estrows[i])):
         line.append(int(estrows[i][j]))
     metro.add_node(nodo, linea=line)
 
-for j in len(frows):  # Añade aristas
+for j in range(0, len(frows)):  # Añade aristas
     first = frows[j][0]
     second = frows[j][1]
     distance = int(frows[j][2])
