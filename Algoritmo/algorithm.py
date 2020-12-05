efb9586f4c4cc9ac2f035bac7fb1d0aa361e4f41
Jose Lopez Lopez <jose.llopez@alumnos.upm.es>
2020-12-05 19:36:25 +0100

    posible algoritmo final

--- Algoritmo/algorithm.py
+++ Algoritmo/algorithm.py
@@ -46,7 +46,7 @@ def definirestructuras ():
         distap = float(distrows[k][2])
         distancias[estfinal].append([estinicial, distap])
 
-
+#Busca las distancias aproximadas entre estaciones
 def buscarestaciones (estacionfin, estacionini):
     res = float
     for cad in distancias[estacionfin]:
@@ -55,12 +55,14 @@ def buscarestaciones (estacionfin, estacionini):
             break
     return res
 
+#Calcula la h de una estacion a la estacion destino
 def h(intermedia, destino, lineaAct):
     n = buscarestaciones(destino, intermedia)
     if lineaAct != lineaCompartida(intermedia, destino):
         n = n + TRANSBORDO;
     return n
 
+#Devuelve la linea que hay entre dos estaciones contiguas
 def lineaCompartida(inicio, final):
     lineasOrigen = metro[inicio]['linea']
     lineasFinal = metro[final]['linea']
@@ -69,6 +71,8 @@ def lineaCompartida(inicio, final):
             return linea
     return 0
 
+#Devuelve el tiempo real entre dos estaciones contiguas en funcion de la
+#linea en la que estabamos
 def g(origen, intermedia, lineaActual):
     coste = metro.edges[origen][intermedia]['time']
     listaLineas = metro[origen, intermedia]['linea']
@@ -80,6 +84,7 @@ def f(origen, intermedia, destino, lineaActual):
     lineaOrInter = lineaCompartida(origen, intermedia)
     return g(origen, intermedia, lineaActual) + h(intermedia, destino, lineaOrInter)
 
+#Encuentra el arbol recubridor de menor peso que contiene a origen y destino
 def findPath(origen, destino):
     activos = PriorityQueue()
     solucion = nx.Graph()
@@ -105,6 +110,7 @@ def findPathRec(nActual, nDestino, activos, solActual, visitados, lineaActual):
     lineaActual = seleccionado[1]
     return findPathRec(siguienteEstacion, nDestino, activos, solActual, visitados, lineaActual)
 
+#Dado un arbol recubridor devuelve el camino que une a origen y destino
 def generarCamino(estOrigen, estDest):
     arbol = findPath(estOrigen, estDest)
     camino = list()
@@ -127,3 +133,20 @@ def generarCaminoRec(estOr, estDest, camino, visitados, arbol):
     camino.pop()
     return None
 
+#Calcula el tiempo que se tarda en recorrer un camino
+def calcularTiempo(camino):
+    lineaActual = lineaCompartida(camino[0], camino[1])
+    tiempo = 0
+    for i in range(len(camino)-1):
+        if lineaActual == lineaCompartida(metro[i], metro[i+1]):
+            tiempo = tiempo + metro.edges[camino[i]][camino[i + 1]]['tiempo']
+        else:
+            tiempo = tiempo + metro.edges[camino[i]][camino[i + 1]]['tiempo'] + TRANSBORDO
+    return tiempo
+
+#Devuelve una lista con las lineas que se han utilizado entre cada estacion del camino
+def lineasUtilizadas(camino):
+    lineas = list()
+    for i in range(len(camino) - 1):
+        lineas.append(lineaCompartida(metro[i], metro[i+1]))
+    return lineas
